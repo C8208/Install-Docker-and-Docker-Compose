2a6dabed2001d975b6e6fd607ac78386dfa47d55
C8208-Eric <81535433+C8208@users.noreply.github.com>
2022-02-23 10:58:24 +0300

    Update install_docker_and_docker_compose.sh

--- install_docker_and_docker_compose.sh
+++ install_docker_and_docker_compose.sh
@@ -1,3 +1,25 @@
+# Setup on Ubuntu
+
+#  Install Docker
+#  Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+#  Set up the repository
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    wget \
+    gnupg \
+    lsb-release \
+    software-properties-common
+
+
+#  Add Docker’s official GPG key:
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -21,3 +43,6 @@ newgrp docker
 #  install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose 
+
+#  Verify
+sudo docker run hello-world
